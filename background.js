7b5d017bb8b544ac3790361a9b74b9da7892f2f2
Tony Tang <tangchang21@gmail.com>
2024-05-11 17:33:39 +0800

    fix lifecycle issue

--- background.js
+++ background.js
@@ -1,19 +1,17 @@
 chrome.runtime.onInstalled.addListener(function () {
-  // console.log("Emacs Helper Installed");
-
   chrome.contextMenus.create({
     id: "copyAsOrgLink",
     title: "Copy as Org Link",
     contexts: ["link"],
   });
+});
 
-  chrome.contextMenus.onClicked.addListener(function (info, tab) {
-    if (info.menuItemId === "copyAsOrgLink") {
-      const link = info.linkUrl;
-      const title = info.selectionText;
-      const orgLink = title ? `[[${link}][${title}]]` : `[[${link}]]`;
-      chrome.tabs.sendMessage(tab.id, { action: 'cb.writeText', data: orgLink, });
-    }
-  });
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  if (info.menuItemId === "copyAsOrgLink") {
+    const link = info.linkUrl;
+    const title = info.selectionText;
+    const orgLink = title ? `[[${link}][${title}]]` : `[[${link}]]`;
+    chrome.tabs.sendMessage(tab.id, { action: 'cb.writeText', data: orgLink, });
+  }
 });
 
